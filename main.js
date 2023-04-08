3d84b3ef9bf64e45bb975f059d052bcf7b1426e5
MORGANJASON <420morganjason@gmail.com>
2023-04-08 21:55:47 +0300

    decrement buy ticket button

--- main.js
+++ main.js
@@ -1,84 +1,10 @@
-
-// fetch("db.json")
-//   .then(response => response.json())
-//   .then(data => {
-//     // 2. Extract details of the first movie
-//     const movie = data.films[0];
-//     const { poster, title, runtime, showtime, capacity, tickets_sold } = movie;
-//     availableTickets = capacity - tickets_sold;    // 3. Set details in the HTML elements
-//     document.getElementById('poster').src = poster;
-//     document.getElementById('title').textContent = title;
-//     document.getElementById('runtime').textContent = `${runtime} mins`;
-//     document.getElementById('showtime').textContent = showtime;
-//     document.getElementById('available-tickets').textContent = availableTickets;    // 4. Create movie list
-//     const filmsList = document.getElementById('films');
-//     data.films.forEach(movie => {
-//       const { id, title, poster, showtime } = movie;
-//       const li = document.createElement('li');
-//       li.innerHTML = `<img src="${poster}" alt="${title}"><div><h4>${title}</h4><p>Showtime: ${showtime}</p></div>`;
-//       li.addEventListener('click', () => showMovieDetails(id));
-//       filmsList.appendChild(li);
-//         });    // 5. Set up buy ticket button click event taking buyTicket as a callback
-//     document.getElementById('buy-ticket').addEventListener('click', buyTicket);
-//     })
-//     .catch(error => {
-//     console.log(error);
-//     });    // buyTicket function decreases the number of available votes by one after every
-//     // click of the Buy Ticket button. The button greys out (is disabled)
-//     // and becomes ineffective once the available votes reaches 0.
-//     function buyTicket() {
-//       if (availableTickets > 0) {
-//         availableTickets--;
-//         document.getElementById('available-tickets').textContent = availableTickets;
-//         if (availableTickets === 0) {
-//           const buyButton = document.getElementById('buy-ticket');
-//           buyButton.textContent = 'Sold Out';
-//           buyButton.disabled = true;
-//         }
-//       }}    // Function to update the movie details
-//     function updateMovieDetails(movie) {
-//       const { poster, title, runtime, showtime, capacity, tickets_sold } = movie;
-//       availableTickets = capacity - tickets_sold;
-//       document.getElementById('poster').src = poster;
-//       document.getElementById('title').textContent = title;
-//       document.getElementById('runtime').textContent = `${runtime} mins`;
-//       document.getElementById('showtime').textContent = showtime;
-//       document.getElementById('available-tickets').textContent = availableTickets;
-//     }
-//     // Function to show movie details when a movie is clicked
-//     function showMovieDetails(id) {
-//       fetch("db.json")
-//         .then(response => response.json())
-//         .then(data => {
-//           const movie = data.films.find(movie => movie.id === id);
-//           if (movie) {
-//             updateMovieDetails(movie);
-//           }
-//         })
-//       .catch(error => console.error(error));
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const postsList = document.querySelector('.post-list');
 const addPostForm = document.querySelector('.add-post-form');
 const titleValue = document.getElementById('title-value');
 const bodyValue = document.getElementById('body-value');
 
-let output = ''
-// const url = 'http://localhost:3000/films'
+let output = ""
+// const url =  "http://localhost:3000/films"
 const renderPosts = (posts) => {
   posts.forEach(post => {
         // console.log(post);
@@ -90,51 +16,173 @@ const renderPosts = (posts) => {
                     <p class="card-text">${post.description}</p>
 
                     <h6 class="card-subtitle mb-2 text-muted">${post.showtime}</h6>
-                      <p class="card-text">${post.capacity} Available tickets</p>
+                      <p class="availableTkts">Available tickets  ${post.capacity-post.tickets_sold} - ${post.capacity}</p>
                         <p class="card-text">${post.tickets_sold} tickets-sold</p>
                         <button class="buy-ticket-btn" data-id="${post.id}">BUY TICKET</button>
         <button class="decrement-ticket-btn" data-id="${post.id}">-</button>
                 </div>
+                 <h5>Tickets Remaining: <span id="ticket-count">10</span></h5>
+                <button onclick="buyTicket()">BUY TICKET</button>
+                    
             </div>
             `;
+            
     });
+//  ticketCountElement.innerText = ticketCount.toString();
 postsList.innerHTML = output;
 
-  // add event listeners to the BUY TICKET and DECREMENT buttons
-  const buyTicketBtns = document.querySelectorAll('.buy-ticket-btn');
-  const decrementBtns = document.querySelectorAll('.decrement-ticket-btn');
-  buyTicketBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      // increment the ticket count for the corresponding movie
-      const postId = btn.dataset.id;
-      const post = posts.find(p => p.id == postId);
-      if (post.capacity > post.tickets_sold) {
-        post.tickets_sold++;
-        renderPosts(posts); // update the page
+
+
+  };
+
+ function buyTicket() {
+        const ticketCountElement = document.getElementById("ticket-count");
+        let ticketCount = parseInt(ticketCountElement.innerText);
+
+        if (ticketCount > 0) {
+          ticketCount--;
+          ticketCountElement.innerText = ticketCount.toString();
+        } else {
+          alert("No more tickets remaining!");
+        }
       }
-    });
-  });
-  decrementBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      // decrement the ticket count for the corresponding movie
-      const postId = btn.dataset.id;
-      const post = posts.find(p => p.id == postId);
-      if (post.tickets_sold > 0) {
-        post.tickets_sold--;
-        renderPosts(posts); // update the page
-      }
-    });
-  });
+
+// Function to handle "Buy Ticket" button click event
+const handleBuyTicket = (event) => {
+   console.log("Buy Ticket button clicked");
+  const postId = event.target.dataset.id;
+  const post = posts.find(post => post.id === postId);
+
+  if (post.capacity - post.tickets_sold <= 0) {
+    alert("Sorry, this showing is sold out.");
+    return;
+  }
+
+  post.tickets_sold++;
+  renderPosts(posts);
 };
 
+// Function to add event listeners to "Buy Ticket" buttons
+const addBuyTicketListeners = () => {
+  const buyTicketButtons = document.querySelectorAll(".buy-ticket-btn");
+  buyTicketButtons.forEach(button => {
+    button.addEventListener("click", handleBuyTicket);
+  });
+   console.log("Buy Ticket event listeners added");
+
+};
+
+// Example posts data
+const posts = [
+  {
+    id: 1,
+    title: "Movie 1",
+    poster: "https://example.com/poster1.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    showtime: "Saturday, April 10, 2023 7:00 PM",
+    capacity: 50,
+    tickets_sold: 0
+  },
+  {
+    id: 2,
+    title: "Movie 2",
+    poster: "https://example.com/poster2.jpg",
+    description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    showtime: "Sunday, April 11, 2023 3:00 PM",
+    capacity: 100,
+    tickets_sold: 50
+  }
+];
+
+  function buyTicket() {
+        const ticketCountElement = document.getElementById("ticket-count");
+        let ticketCount = parseInt(ticketCountElement.innerText);
+
+        if (ticketCount > 0) {
+          ticketCount--;
+          ticketCountElement.innerText = ticketCount.toString();
+        } else {
+          alert("No more tickets remaining!");
+        }
+      }
+
+// Render posts on the frontend and add event listeners to "Buy Ticket" buttons
+renderPosts(posts);
+addBuyTicketListeners();
+
 // fetch the movie data from the server and render it on the page
-fetch('http://localhost:3000/films')
+fetch("http://localhost:3000/films")
   .then(res => res.json())
   .then(data => {
     // initialize the tickets_sold property for each movie to 0
     data.forEach(post => post.tickets_sold = 0);
     renderPosts(data);
-  });
+
+
+
+
+
+
+      // Function to show movie details when a movie is clicked
+    function showMovieDetails(id) {
+      fetch("http://localhost:3000/films")
+        .then(response => response.json())
+        .then(data => {
+          const movie = data.films.find(movie => movie.id === id);
+          if (movie) {
+            updateMovieDetails(movie);
+          }
+        })
+      .catch(error => console.error(error));
+
+
+      
+
+      }
+
+      //     console.log(error);
+    });  
+    
+    const ticketCount = document.getElementById("ticket-count");
+const buyBtn = document.getElementById("buy-btn");
+const removeBtn = document.getElementById("remove-btn");
+
+let availableTickets = 50;
+
+buyBtn.addEventListener("click", function() {
+  if (availableTickets > 0) {
+    availableTickets--;
+    ticketCount.textContent = availableTickets;
+  } else {
+    alert("No more tickets available!");
+  }
+});
+
+removeBtn.addEventListener("click", function() {
+  availableTickets++;
+  ticketCount.textContent = availableTickets;
+});
+
+    
+    
+    
+    
+    
+    
+    // buyTicket function decreases the number of available votes by one after every
+//     // click of the Buy Ticket button. The button greys out (is disabled)
+//     // and becomes ineffective once the available votes reaches 0.
+//     function buyTicket() {
+//       if (availableTickets > 0) {
+//         availableTickets--;
+//         document.getElementById('available-tickets').textContent = availableTickets;
+//         if (availableTickets === 0) {
+//           const buyButton = document.getElementById('buy-ticket');
+//           buyButton.textContent = 'Sold Out';
+//           buyButton.disabled = true;
+//         }
+// }
+  // });
 
 // // Get - read the posts
 // // method:GET
